935ee5c0f9786f8a016fa6bf4175c7d5e160dcb3
Bradley <bpoulett@ualberta.ca>
2016-03-10 13:45:22 -0700

    Added sql setup script

--- main.go
+++ main.go
@@ -1,24 +1,74 @@
 package main
 
 import (
-    "log"
-    "os"
     "database/sql"
+    "fmt"
     _ "github.com/lib/pq"
+    "time"
 )
 
-var (
-    db     *sql.DB = nil
-)
-
-func main() {
-    startDatabase()
+const{
+	DB_USER = "veqkxyunaxkmbq"
+	DB_PWD = "rZ3bzxlYEIgPXhnNw4lUKwVPzW"
+	DB_NAME = "d4smpakov9rj44"
 }
 
-func startDatabase(){
-    var errd error
-	db, errd = sql.Open("postgres", os.Getenv("DATABASE_URL"))
-    if errd != nil {
-        log.Fatalf("Error opening database: %q", errd)
+func main() {
+    dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+        DB_USER, DB_PASSWORD, DB_NAME)
+    db, err := sql.Open("postgres", dbinfo)
+    checkErr(err)
+    defer db.Close()
+
+    fmt.Println("# Inserting values")
+
+    var lastInsertId int
+    err = db.QueryRow("INSERT INTO userinfo(username,departname,created) VALUES($1,$2,$3) returning uid;", "astaxie", "研发部门", "2012-12-09").Scan(&lastInsertId)
+    checkErr(err)
+    fmt.Println("last inserted id =", lastInsertId)
+
+    fmt.Println("# Updating")
+    stmt, err := db.Prepare("update userinfo set username=$1 where uid=$2")
+    checkErr(err)
+
+    res, err := stmt.Exec("astaxieupdate", lastInsertId)
+    checkErr(err)
+
+    affect, err := res.RowsAffected()
+    checkErr(err)
+
+    fmt.Println(affect, "rows changed")
+
+    fmt.Println("# Querying")
+    rows, err := db.Query("SELECT * FROM userinfo")
+    checkErr(err)
+
+    for rows.Next() {
+        var uid int
+        var username string
+        var department string
+        var created time.Time
+        err = rows.Scan(&uid, &username, &department, &created)
+        checkErr(err)
+        fmt.Println("uid | username | department | created ")
+        fmt.Printf("%3v | %8v | %6v | %6v\n", uid, username, department, created)
+    }
+
+    fmt.Println("# Deleting")
+    stmt, err = db.Prepare("delete from userinfo where uid=$1")
+    checkErr(err)
+
+    res, err = stmt.Exec(lastInsertId)
+    checkErr(err)
+
+    affect, err = res.RowsAffected()
+    checkErr(err)
+
+    fmt.Println(affect, "rows changed")
+}
+
+func checkErr(err error) {
+    if err != nil {
+        panic(err)
     }
 }
--- setup.sql
+++ setup.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE tablespacedb;
 USE tablespacedb;
 
-CREATE TABLE fooditem{
+CREATE TABLE fooditem(
 	id INT NOT NULL PRIMARY KEY,
 	name varchar(50) NOT NULL,
 	price money NOT NULL,
@@ -10,21 +10,21 @@ CREATE TABLE fooditem{
 	isactive boolean NOT NULL,
 	omnivoreid varchar(100),
 	itempicture varchar(1000)
-};
+);
 
-CREATE TABLE tag{
+CREATE TABLE tag(
 	id INT NOT NULL PRIMARY KEY,
 	description varchar(1000),
 	name varchar(100) NOT NULL
-};
+);
 
-CREATE TABLE foodtags{
+CREATE TABLE foodtags(
 	foodid INT NOT NULL,
 	tagid INT NOT NULL,
 	CONSTRAINT pk_foodTag PRIMARY KEY (foodid,tagid)
-};
+);
 
-CREATE TABLE restaurant{
+CREATE TABLE restaurant(
 	id INT NOT NULL PRIMARY KEY,
 	name varchar(500) NOT NULL, 
 	logo varchar(1000),
@@ -32,26 +32,26 @@ CREATE TABLE restaurant{
 	phonenumber varchar(20),
 	email varchar(50) NOT NULL,
 	hours varchar (500)
-};
+);
 
-CREATE TABLE menu{
+CREATE TABLE menu(
 	id INT NOT NULL PRIMARY KEY,
 	isactive boolean
-};
+);
 
-CREATE TABLE menuitems{
+CREATE TABLE menuitems(
 	menuid INT NOT NULL,
 	foodid INT NOT NULL,
 	CONSTRAINT pk_menuitem PRIMARY KEY (menuid, foodid)
-};
+);
 
-CREATE TABLE restaurantmenus{
+CREATE TABLE restaurantmenus(
 	restaurantid INT NOT NULL,
 	menuid INT NOT NULL,
 	CONSTRAINT pk_restaurantmenu PRIMARY KEY (restaurantid, menuid)
-};
+);
 
-CREATE TABLE users{
+CREATE TABLE users(
 	id INT NOT NULL PRIMARY KEY,
 	joined date NOT NULL,
 	lastactive date NOT NULL,
@@ -60,80 +60,80 @@ CREATE TABLE users{
 	lastname varchar(50),
 	address varchar (100),
 	email varchar(500) NOT NULL
-};
+);
 
-CREATE TABLE passwords{
+CREATE TABLE passwords(
 	userid INT NOT NULL PRIMARY KEY,
 	hash varchar(50) NOT NULL,
 	salt varchar(50) NOT NULL
-};
+);
 
-CREATE TABLE restrictions{
+CREATE TABLE restrictions(
 	userid INT NOT NULL,
 	tagid INT NOT NULL,
 	CONSTRAINT pk_restriction PRIMARY KEY (userid, tagid)
-};
+);
 
-CREATE TABLE favourites{
+CREATE TABLE favourites(
 	userid INT NOT NULL,
 	foodid INT NOT NULL,
 	CONSTRAINT pk_favourite PRIMARY KEY (userid, foodid)
-};
+);
 
-CREATE TABLE accesslevels{
+CREATE TABLE accesslevels(
 	accessleveid INT NOT NULL PRIMARY KEY,
 	description varchar(100),
 	name varchar(50) NOT NULL
-};
+);
 
-CREATE TABLE permissions{
+CREATE TABLE permissions(
 	userid INT NOT NULL,
 	restaurantid INT NOT NULL,
 	accesslevelid INT NOT NULL,
 	CONSTRAINT pk_permission PRIMARY KEY (userid, restaurantid)
-};
+);
 
-CREATE TABLE paymenttypes{
+CREATE TABLE paymenttypes(
 	id INT NOT NULL PRIMARY KEY,
 	description varchar(1000),
 	name varchar(50) NOT NULL
-};
+);
 
-CREATE TABLE userpaymenttypes{
+CREATE TABLE userpaymenttypes(
 	userid INT NOT NULL,
 	typeid INT NOT NULL,
 	CONSTRAINT pk_userpaymenttype PRIMARY KEY (userid, typeid)
-};
+);
 
-CREATE TABLE transactions{
+CREATE TABLE transactions(
 	id INT NOT NULL PRIMARY KEY,
 	paymenttypeid INT NOT NULL,
 	totalprice money NOT NULL,
 	transactiondate date NOT NULL	
-};
+);
 
-CREATE TABLE orders{
+CREATE TABLE orders(
 	id INT NOT NULL PRIMARY KEY,
 	creationtime date NOT NULL,
 	updatetime date,
 	statusid INT NOT NULL,
 	restaurantid INT NOT NULL,
 	total money NOT NULL,
-};
+);
 
-CREATE TABLE ordertransactions{
+CREATE TABLE ordertransactions(
 	orderid INT NOT NULL,
 	transactionid INT NOT NULL,
 	CONSTRAINT pk_ordertransaction PRIMARY KEY(orderid, transactionid)
-};
+);
 
-CREATE TABLE statuses{
+CREATE TABLE statuses(
 	id INT NOT NULL PRIMARY KEY,
 	name varchar(50) NOT NULL
-};
+);
 
-CREATE TABLE ordercontents{
+CREATE TABLE ordercontents(
 	orderid INT NOT NULL,
 	foodid INT NOT NULL,
 	CONSTRAINT pk_ordercontents PRIMARY KEY (orderid, foodid)
-};
+);
