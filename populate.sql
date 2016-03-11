794494510250d42f37d14c5f028d862f962ed284
Bradley <bpoulett@ualberta.ca>
2016-03-10 18:03:57 -0700

    Added godeps for gi

---
+++ populate.sql
@@ -0,0 +1,92 @@
+
+INSERT INTO fooditem VALUES ( 000002, 'Chicken Fingers', 'Original or Buffalo
+style. Lightly breaded chicken breast filet served with your choice of dipping
+sauce.', TRUE, NULL, 'http://i.imgur.com/ecnHEVh.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000003, 'Classic Nachos', 'Hand-cut tortilla chips layered with cheddar and pizza mozzarella, topped with black olives, green peppers, tomatoes and jalapeño peppers. Served with salsa and sour cream on the side.Due to the nature of this item, we recommend it be enjoyed in the restaurant and do not offer it for Take-out or Delivery.', TRUE, NULL, 'http://i.imgur.com/tIUUdLB.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000004, 'Fried Wings', 'Fried until golden and crispy."', TRUE, NULL, 'http://i.imgur.com/Zckbqza.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000005, 'Garlic Toast', 'Fresh from the oven, brushed with butter and garlic.', TRUE, NULL, 'http://i.imgur.com/s4YvH87.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000006, 'Salt and Pepper Dry Ribs', 'Tender pork ribs fried and tossed in a salt & pepper dry rub and finished with chopped red peppers.', TRUE, NULL, 'http://i.imgur.com/27bgJWH.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000007, 'Three Cheese Bread', 'Loaded with bubbling cheddar, pizza mozzarella and Parmesan with a hint of garlic.', TRUE, NULL, 'http://i.imgur.com/3we7dRQ.jpg', 10.00, 0.00); 
+
+INSERT INTO menuitems VALUES (000001, 000001);
+INSERT INTO menuitems VALUES (000001, 000002);
+INSERT INTO menuitems VALUES (000001, 000003);
+INSERT INTO menuitems VALUES (000001, 000004);
+INSERT INTO menuitems VALUES (000001, 000005);
+INSERT INTO menuitems VALUES (000001, 000006);
+INSERT INTO menuitems VALUES (000001, 000007);
+
+INSERT INTO menu VALUES ( 000002, TRUE, 'Entrees' );
+
+INSERT INTO fooditem VALUES ( 000008, 'Beef Dip', 'It doesn’t get any better than our reimagined beef dip. Thinly sliced steak piled high on a toasted garlic-brushed bun with melted pizza mozzarella, braised onions, roasted red peppers and crispy onion straws. Served with horseradish mayo and jus for dipping.', TRUE, NULL, 'http://i.imgur.com/slqaMuK.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000009, 'Prime Rib Burger', '100% Canadian half-pound prime rib beef burger, fresh lettuce, tomato, red onion and pickles. Plus, our secret ingredient - a zesty sauce that simply defies description. Recommended for burger connoisseurs.', TRUE, NULL, 'http://i.imgur.com/99hU5nq.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000010, 'Chicken Quesadilla', 'A tortilla stuffed with tender chicken, sun-dried tomatoes, cheddar, pizza mozzarella, sun-dried tomato pesto and tangy chipotle sauce, then brushed with garlic spread and Parmesan and oven baked to golden perfection. Served with sour cream and salsa for dipping.', TRUE, NULL, 'http://i.imgur.com/ww9owCc.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000011, 'Chicken Parmesan', 'A tender chicken breast, breaded and fried until golden brown, topped with our signature marinara sauce and pizza mozzarella, then baked to perfection. Served with garlic toast, seasonal vegetables and your choice of side dish.', TRUE, NULL, 'http://i.imgur.com/AIQ5AMR.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000012, 'Ribs and Wings', 'Succulent, oh-so-slowly roasted pork back ribs baked in your choice of our signature BBQ, honey garlic, Jack Daniel’s® BBQ or whisky BBQ sauce, paired with Bostons famous wings prepared just the way you like them. Served with garlic toast, seasonal vegetables and your choice of side dish.', TRUE, NULL, 'http://i.imgur.com/oQc38mn.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000013, 'Steak and Veggies', 'A 7-ounce New York strip loin steak, aged a minimum of 28 days to the peak of tenderness and flavour, charbroiled the way you like it. Served with seasonal vegetables and garden greens with low-fat poppy seed dressing. Sodium: 333 mg, Protein: 44 g, Calories: 446, Saturated fat: 7.8 g, Total fat: 22.9 g. Due to the nature of this item, we recommend it be enjoyed in the restaurant and do not offer it for Take-out or Delivery.', TRUE, NULL, 'http://i.imgur.com/dJGe3TQ.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000014, 'Crispy Chicken Pecan Salad', 'Breaded chicken breast filet, toasted pecans, pizza mozzarella, cheddar, smoky bacon, chopped egg, julienne carrots and diced tomato over fresh garden greens tossed in ranch dressing.', TRUE, NULL, 'http://i.imgur.com/N9UbSl3.jpg', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000015, 'Garden Salad', 'A blend of garden and artisan greens served with our house balsamic vinaigrette. Starter size only.', TRUE, NULL, 'http://i.imgur.com/zzTLX3K.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000016, 'Mediterranean Salad', 'Our twist on a favourite. Chunks of cucumbers, red onions, red peppers and tomatoes tossed in a Mediterranean vinaigrette with feta and kalamata olives on a bed of garden greens.', TRUE, NULL, 'http://i.imgur.com/57z7g6I.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000017, 'Caesar Salad', 'Romaine lettuce tossed with our signature creamy garlic dressing and crunchy croutons, sprinkled with freshly grated Parmesan.', TRUE, NULL, 'http://i.imgur.com/gaZKa8C.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000018, 'Jambalaya Fettucini', 'Fettuccini tossed in our signature marinara sauce with tender chicken breast, shrimp, spicy Italian sausage, tomatoes, green peppers and topped with green onions.', TRUE, NULL, 'http://i.imgur.com/LJohYcU.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000019, 'Lasagna', 'Our beefy Bolognese sauce layered over tender noodles, topped with pizza mozzarella, then baked to perfection and finished with fresh Parmesan.', TRUE, NULL, 'http://i.imgur.com/UjvsDSS.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000020, 'Chicken and Mushroom Fettuccini', 'Tender sautéed chicken breast and fresh mushrooms tossed in our signature Marsala cream sauce with fettuccini, tomatoes and green onion.', TRUE, NULL, 'http://i.imgur.com/6STbupK.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000021, 'Deluxe Pizza', 'Signature pizza sauce, pizza mozzarella, pepperoni, smoked ham, green pepper and fresh mushrooms.', TRUE, NULL, 'http://i.imgur.com/yTq4FE9.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000022, 'Pepperoni Pizza', 'Signature pizza sauce, pizza mozzarella and loads of pepperoni.', TRUE, NULL, 'http://i.imgur.com/cO8saJT.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000023, 'Vegetarian Pizza', 'Signature pizza sauce, pizza mozzarella, fresh mushrooms, green peppers and onions, topped with fresh tomato slices. For a lighter option try it as a small multrigrain thin crust.', TRUE, NULL, 'http://i.imgur.com/GZowsYY.png', 10.00, 0.00); 
+
+INSERT INTO menuitems VALUES (000002, 000008);
+INSERT INTO menuitems VALUES (000002, 000009);
+INSERT INTO menuitems VALUES (000002, 000010);
+INSERT INTO menuitems VALUES (000002, 000011);
+INSERT INTO menuitems VALUES (000002, 000012);
+INSERT INTO menuitems VALUES (000002, 000013);
+INSERT INTO menuitems VALUES (000002, 000014);
+INSERT INTO menuitems VALUES (000002, 000015);
+INSERT INTO menuitems VALUES (000002, 000016);
+INSERT INTO menuitems VALUES (000002, 000017);
+INSERT INTO menuitems VALUES (000002, 000018);
+INSERT INTO menuitems VALUES (000002, 000019);
+INSERT INTO menuitems VALUES (000002, 000020);
+INSERT INTO menuitems VALUES (000002, 000021);
+INSERT INTO menuitems VALUES (000002, 000022);
+INSERT INTO menuitems VALUES (000002, 000023);
+
+
+INSERT INTO menu VALUES ( 000003, TRUE, 'Desserts' );
+
+INSERT INTO fooditem VALUES ( 000024, 'Apple Crisp', 'Just like mom used to make it. Slices of sweet granny smith apples in a butter sauce, baked to a crisp with a delicious brown sugar and oat crust. Topped with caramel sauce and served with a scoop of vanilla bean ice cream.Due to the nature of this item, we recommend it be enjoyed in the restaurant and do not offer it for Take-out or Delivery.', TRUE, NULL, 'http://i.imgur.com/6dwDy5X.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000025, 'Brownie', 'A warm chocolate brownie topped with two scoops of vanilla bean ice cream, caramel and chocolate sauce.Due to the nature of this item, we recommend it be enjoyed in the restaurant and do not offer it for Take-out or Delivery.', TRUE, NULL, 'http://i.imgur.com/BfxhW3S.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000026, 'Chocolate Cake', 'Ultimate decadence. Creamy chocolate mousse with chunks of cheesecake, caramel, toffee, pecans and almonds on a chocolate crust.', TRUE, NULL, 'http://i.imgur.com/crQ4BM4.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000027, 'New York Cheesecake', 'A cheesecake lover’s delight. Traditional style creamy cheesecake piled high on a graham cracker crust.', TRUE, NULL, 'http://i.imgur.com/eXkQcSn.png', 10.00, 0.00); 
+
+INSERT INTO menuitems VALUES (000003, 000024);
+INSERT INTO menuitems VALUES (000003, 000025);
+INSERT INTO menuitems VALUES (000003, 000026);
+INSERT INTO menuitems VALUES (000003, 000027);
+
+
+INSERT INTO menu VALUES ( 000004, TRUE, 'Drinks' );
+
+INSERT INTO fooditem VALUES ( 000028, 'Milk', '', TRUE, NULL, 'http://i.imgur.com/0CwIREH.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000029, 'Chocolate Milk', '', TRUE, NULL, 'http://i.imgur.com/T4ITVzS.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000030, '12oz Can of Pop', '7Up®, Pepsi®, Diet Pepsi®, MUG® Root Beer, Brisk® Iced Tea 12 Oz can. Delivery / Pick-Up only.', TRUE, NULL, 'http://i.imgur.com/DA7z2MS.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000031, 'Fruit Juice', 'Choose from apple or orange juice.', TRUE, NULL, 'http://i.imgur.com/3J6ecRx.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000032, 'Hot Chocolate', 'Nice and sweet', TRUE, NULL, 'http://i.imgur.com/g86LTLr.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000033, 'Starbucks Pike Place Coffee', 'A smooth, well-rounded blend of Latin American coffees. Pike Place is a registered trademark of The Pike Place Market PDA, used under license.', TRUE, NULL, 'http://i.imgur.com/Anovd2H.png', 10.00, 0.00); 
+INSERT INTO fooditem VALUES ( 000034, 'Tazo® Teas', 'Ask your server for our current available selection of premium Tazo® Teas including black, green and herbal infusions.', TRUE, NULL, 'http://i.imgur.com/PMkN2Yt.png', 10.00, 0.00); 
+
+INSERT INTO menuitems VALUES (000004, 000028);
+INSERT INTO menuitems VALUES (000004, 000029);
+INSERT INTO menuitems VALUES (000004, 000030);
+INSERT INTO menuitems VALUES (000004, 000031);
+INSERT INTO menuitems VALUES (000004, 000032);
+INSERT INTO menuitems VALUES (000004, 000033);
+INSERT INTO menuitems VALUES (000004, 000034);
+
+INSERT INTO restaurant VALUES (000001, 'La Pasta Trattoria', NULL, NULL, NULL, 'temporary@gmail.com', NULL);
+
+INSERT INTO restaurantmenus VALUES (000001, 000001);
+INSERT INTO restaurantmenus VALUES (000001, 000002);
+INSERT INTO restaurantmenus VALUES (000001, 000003);
+INSERT INTO restaurantmenus VALUES (000001, 000004);
---
+++ temp_test.sql
@@ -0,0 +1,21 @@
+﻿SELECT * FROM d4smpakov9rj44.information_schema.tables WHERE table_schema = 'public';
+
+SELECT * FROM fooditem;
+
+SELECT * from menuitems;
+
+SELECT * FROM restaurant;
+/*
+SELECT * FROM menuitems;
+
+SELECT * FROM fooditem;
+
+
+INSERT INTO fooditem VALUES ( 000001, 'Cactus Cuts', 'Spicy and thinly sliced fried potatoes served with our mouth-watering cactus dip - a rich blend of crushed chilies, green onion, sour cream and Parmesan.', TRUE, NULL, 'http://i.imgur.com/P8ua9ut.png', 10.00, 0.00);
+
+SELECT * FROM menu;
+
+
+ALTER TABLE fooditem DROP COLUMN discount;
+
+ALTER TABLE fooditem ADD COLUMN discount decimal;*/
